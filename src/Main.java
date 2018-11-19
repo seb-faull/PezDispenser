194cca1764d254dc070c228e6b20ec20e2bbb5d1
bxxstian <seb_faull@hotmail.co.uk>
2018-11-19 18:30:08 +0000

    Implement encapsulation and final static properties

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,23 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("We are making a new Pez Dispenser");
+        System.out.printf("FUN FACT: There are %d PEZ allowed in every dispenser %n",
+                            PezDispenser.MAX_PEZ);
         PezDispenser dispenser = new PezDispenser("Yoda");
         System.out.printf("The dispenser is %s %n",
                             dispenser.getCharacterName()
                         );
+
+        if (dispenser.isEmpty()) {
+            System.out.println("Dispenser is empty");
+        }
+        System.out.println("Filling the dispenser with delicious PEZ...");
+        dispenser.fill();
+        if (!dispenser.isEmpty()) {
+            System.out.println("Dispenser is full");
+        }
+
+
 
     }
 
--- src/PezDispenser.java
+++ src/PezDispenser.java
@@ -1,17 +1,31 @@
 public class PezDispenser {
+    // final means you can't change the state once it's been initialised
+    public static final int MAX_PEZ = 12;
+    private int pezCount;
+
     // private String characterName = "Yoda";
     final private String characterName;
 
     // Constructor method - notice the method name is the same as the class name
     public PezDispenser(String characterName) {
         this.characterName = characterName;
+        pezCount = 0;
+    }
+
+    public void fill() {
+        pezCount = MAX_PEZ;
+    }
+
+    public boolean isEmpty() {
+        return pezCount == 0;
     }
 
     public String getCharacterName() {
         return characterName;
     }
 
-/*  -- ANOTHER CODING STYLE --
+/*
+    -- ANOTHER CODING STYLE --
     private String mCharacterName;
 
     // Constructor method - notice the method name is the same as name class name
